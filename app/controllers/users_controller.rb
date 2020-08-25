403799aa44e392918c11be7f7170eff2882edb2f
Anton Krestyaninov <9593795@gmail.com>
2020-08-25 04:20:55 +0300

    Добавил простую аутентификацию через session Сейчас работают только регистрация и вход Добавил страницы регистрации и входа

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'You signed up successfully'
+    else
+      flash.now[:alert] = 'Sorry, try again!'
+      render :new
+    end
+  end
+
+  def show
+  end
+end
+
+private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
