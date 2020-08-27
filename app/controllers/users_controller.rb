a1f2750030c6f73d32bc1d9398b33a23d04b7303
Anton Krestyaninov <9593795@gmail.com>
2020-08-27 23:09:36 +0300

    Добавил стили для страницы входа Отформатировал код так, чтобы по возможности строки помещались в 80 символов

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,3 @@
 class UsersController < ApplicationController
-  # before_action :redirect_if_not_logged_in, except: [:new, :create]
-  # before_action :redirect_if_logged_in,     only:   [:new, :create]
-
-  # def new
-  #   @user = User.new
-  # end
-
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     log_in(@user)
-  #     redirect_to user_url(current_user[:id]),
-  #       notice: 'You signed up successfully'
-  #   else
-  #     flash.now[:alert] = 'Sorry, try again!'
-  #     render :new
-  #   end
-  # end
-
   def show; end
 end
-
-# private
-#   def user_params
-#     params.require(:user).permit(:first_name, :last_name, :email, :password)
-#   end
