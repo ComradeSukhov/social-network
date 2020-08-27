0a91644628c6fe8a540b8fac60a92ca690c3d68d
Anton Krestyaninov <9593795@gmail.com>
2020-08-28 02:19:55 +0300

    В мобильной версии сайта добавил выдвигающееся меню в навбаре Сделал корневую страницу доступной только для не вошедших на сайт пользователей

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,10 @@
 class HomeController < ApplicationController
+  before_action :allow_only_not_signed_in_users
+
   def index; end
 end
+
+private
+  def allow_only_not_signed_in_users
+    redirect_to user_path(current_user) if user_signed_in?
+  end
