7b3a553d336b5189ec8818163f4099f48ab3db80
Anton Krestyaninov <9593795@gmail.com>
2020-08-28 19:09:05 +0300

    Пофиксил неработающий тест

--- app/controllers/welcome_pages_controller.rb
+++ app/controllers/welcome_pages_controller.rb
@@ -2,9 +2,9 @@ class WelcomePagesController < ApplicationController
   before_action :allow_only_not_signed_in_users
 
   def show; end
-end
 
-private
-  def allow_only_not_signed_in_users
-    redirect_to user_path(current_user) if user_signed_in?
-  end
+  private
+    def allow_only_not_signed_in_users
+      redirect_to user_path(current_user) if user_signed_in?
+    end
+end
