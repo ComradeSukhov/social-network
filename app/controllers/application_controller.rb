b8442a2a1a01695445e5fb55c93904306f48d562
Anton Krestyaninov <9593795@gmail.com>
2020-09-02 01:53:58 +0300

    Исправил коллбэк, который запрещал доступ к странице приветствия для всех

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, unless: :devise_controller?
+  before_action :authenticate_user!, unless: :auth_is_not_required?
 
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,
                                                          :last_name])
+    end
+
+  private
+    def auth_is_not_required?
+      devise_controller? || controller_name == 'welcome_pages'
     end
 end
