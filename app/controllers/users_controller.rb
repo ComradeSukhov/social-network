ce595db16ef292f1dd5e6f114a78f4a6ec3871c4
Anton Krestyaninov <9593795@gmail.com>
2020-09-05 00:25:52 +0300

    Решил N+1 проблему при рендере постов на стене

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,33 @@
 class UsersController < ApplicationController
-  include UsersHelper
-
   before_action :set_user, only: [:show]
 
   def show
     @wall = @user.wall
-    @name = full_name(@user)
 
+    # ===> This part of code is related to the post creation form
     if flash[:failed_post]
       # If a user already have tried to post and have failed validations
       #   flash contains all neccessary information about that failed post
       @post = Post.new(flash[:failed_post])
     else
-      @post         = current_user.posts.new
-      @post.wall_id = @wall.id
+      @post = current_user.posts.new(wall_id: @wall.id)
     end
+    # <=== end
+
+    # Get all posts on the wall
+    #   and all neccessary information to display each post
+    @posts = Post.left_outer_joins(:author, :comments)
+                 .select(
+                   :id,
+                   :body,
+                   :created_at,
+                   :author_id,
+                   "users.first_name || ' ' || users.last_name AS author_name",
+                   'count(comments.id) AS comments_quantity')
+                 .group(:id, :first_name, :last_name)
+                 .where(wall_id: @wall.id)
+                 .order(:created_at)
+                 .reverse_order
   end
 
   private
