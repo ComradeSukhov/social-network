21e84227f00e5869619c7c2e145e53254c6bfdad
Anton Krestyaninov <9593795@gmail.com>
2020-09-05 04:04:48 +0300

    Исправил N+1 проблему при рендере постов на стене Исправил ошибку при которой комментарии после удаления уезжали в низ страницы Немного обновил стили на странице с комментариями

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def display_year(obj)
+  def insert_year(obj)
     obj.created_at.year != Time.current.year ? ' %Y ' : ' '
   end
 
   def creation_time(obj)
-    obj.created_at.strftime("%d %b#{ display_year(obj) }at %H:%M")
+    obj.created_at.strftime("%d %b#{ insert_year(obj) }at %H:%M")
   end
 end
