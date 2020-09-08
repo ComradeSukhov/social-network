db479759f212cebf69e489655c384dbdd0f4d898
Anton Krestyaninov <9593795@gmail.com>
2020-09-08 21:54:25 +0300

    Обновил request-тесты для /welcome_page

--- spec/requests/welcome_pages_request_spec.rb
+++ spec/requests/welcome_pages_request_spec.rb
@@ -1,20 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "/welcome_page", type: :request do
+  before(:context) do
+    @user = create(:user)
+  end
+
+  after(:context) do
+    @user.wall.destroy
+    @user.destroy
+  end
+
   describe "GET /show" do
     context 'when not logged in' do
-      it "it responds with success" do
+      before(:example) do
         get welcome_page_path
+      end
+
+      it "displays no aside block" do
         expect(response).to have_http_status :success
+        assert_select('aside', false)
       end
     end
 
     context 'when logged in' do
-      let(:user) { create(:user) }
+      before(:example) do
+        sign_in @user
+        get welcome_page_path
+      end
 
       it "it responds with redirect" do
-        sign_in user
-        get welcome_page_path
         expect(response).to have_http_status :redirect
       end
     end
