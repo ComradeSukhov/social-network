e02624f7ea79ed178a29d00e4dedc2943d60981b
Anton Krestyaninov <9593795@gmail.com>
2020-09-08 09:25:05 +0300

    Обновил существующие тесты

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "/users", type: :request do
   describe "GET /show" do
-
     context 'when not logged in' do
-      it "is expected to respond with redirect" do
+      it "it responds with redirect" do
         get user_path(rand(1..1000))
         expect(response).to have_http_status(:redirect)
         expect(response).to redirect_to(new_user_session_path)
@@ -13,12 +12,32 @@ RSpec.describe "/users", type: :request do
 
     context 'when logged in' do
       let(:user) { create(:regular_user) }
-      it "is expected to respond with success" do
+
+      it "responds with success" do
         sign_in user
         get user_path(user)
         expect(response).to have_http_status(:success)
       end
     end
+  end
 
+  describe "GET /show" do
+    context 'when not logged in' do
+      it "it responds with redirect" do
+        get users_path
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context 'when logged in' do
+      let(:user) { create(:regular_user) }
+
+      it "responds with success" do
+        sign_in user
+        get users_path
+        expect(response).to have_http_status(:success)
+      end
+    end
   end
 end
