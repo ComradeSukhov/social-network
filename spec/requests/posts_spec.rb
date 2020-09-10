a6f3b3564d9928943cc6aa2705dcd026e9fd2cf8
Anton Krestyaninov <9593795@gmail.com>
2020-09-11 01:58:48 +0300

    Добавил больше request-тестов для постов Внёс небольшие корректировки во вьюхи постов

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,18 +1,8 @@
 require 'rails_helper'
+include ApplicationHelper
+include UsersHelper
 
 RSpec.describe 'Posts', type: :request do
-  include ApplicationHelper
-  include UsersHelper
-
-  before(:context) do
-    @user = create(:user)
-  end
-
-  after(:context) do
-    @user.wall.destroy
-    @user.destroy
-  end
-
   context 'when not logged in' do
     describe 'all requests' do
       it 'respond with redirect' do
@@ -31,25 +21,71 @@ RSpec.describe 'Posts', type: :request do
     end
   end
 
-
   context 'when logged in' do
     before(:context) do
+      @user = create(:user)
+    end
+
+    after(:context) do
+      @user.wall.destroy
+      @user.destroy
+    end
+
+    before(:example) do
       sign_in @user
+      @post_attributes = attributes_for(:post,
+                                         author_id: @user.id,
+                                           user_id: @user.id,
+                                           wall_id: @user.wall.id)
+    end
+
+    after(:example) do
+      @post.destroy if @post
     end
 
     describe 'GET /posts/:id' do
-      before(:example) do
-        @post = @user.posts.create(attributes_for(:post,
-                                                   wall_id: @user.wall.id))
+      it "displays a post's page" do
+        @post_attributes.delete(:user_id)
+        @post = Post.create(@post_attributes)
+
         get post_path(@post)
+
+        expect(response).to have_http_status(200)
+        assert_select('p', text: /#{@post.body}/)
+      end
+    end
+
+    describe 'POST /posts' do
+      before(:example) do
+        get user_path(@user)
+        expect(response).to have_http_status(200)
       end
 
-      it "displays a post's page" do
-        expect(response).to have_http_status(:success)
-        assert_select('.post-container', 1)
-        assert_select('.post-container a', text: full_name(@post.author))
-        assert_select('.post-container', text: /#{creation_time(@post)}/)
-        assert_select('.post-container', text: /#{@post.body}/)
+      context 'when post have incorrect data' do
+        it 'displays an error message' do
+          @post_attributes.delete(:body)
+
+          expect { post posts_path, params: { post: @post_attributes } }
+            .not_to change { Post.count }
+          expect(response).to have_http_status(302)
+          expect(flash[:failed_post_errors]).to be_present
+
+          follow_redirect!
+          expect(response).to have_http_status(200)
+          assert_select('.notification.is-danger')
+        end
+      end
+
+      context 'when post have correct data' do
+        it 'displays a page includeing new post' do
+          expect { post posts_path, params: { post: @post_attributes } }
+            .to change { Post.count }.by 1
+          expect(response).to have_http_status(302)
+
+          follow_redirect!
+          expect(response).to have_http_status(200)
+          assert_select('p', text: @post_attributes[:body])
+        end
       end
     end
   end
