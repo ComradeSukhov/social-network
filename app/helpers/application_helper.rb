0eed2718b81239bc54ec84f68f239a8c6fff6a97
Anton Krestyaninov <9593795@gmail.com>
2020-09-17 04:46:50 +0300

    Добавил тесты для хелперов

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def insert_year(obj)
-    obj.created_at.year != Time.current.year ? ' %Y ' : ' '
+    obj.created_at.year == Time.now.year ? ' ' : ' %Y '
   end
 
   def creation_time(obj)
