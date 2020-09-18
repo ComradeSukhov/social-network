1621c63ad970c8f15323563471dcac00729d22b3
Anton Krestyaninov <9593795@gmail.com>
2020-09-18 23:02:38 +0300

    Больше рефакторинга богу рефакторинга! Request-тесты юзеров перегруппированы

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,43 +2,47 @@ require 'rails_helper'
 require_relative '../support/helpers/user_creation'
 
 RSpec.describe 'Users', type: :request do
-  context 'when not logged in' do
-    after(:example) do
-      expect(response).to have_http_status(302)
-      expect(response).to redirect_to(new_user_session_path)
-    end
+  include_context 'create a user'
 
-    describe 'GET /users/:id' do
+  describe 'GET /users/:id' do
+    context 'when not logged in' do
       it 'responds with redirect' do
         get user_path(0)
+
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
 
-    describe 'GET /users' do
-      it 'responds with redirect' do
-        get users_path
+    context 'when logged in' do
+      before(:example) do
+        sign_in @user
       end
-    end
-  end
 
-  context 'when logged in' do
-    include_context 'create a user'
-
-    before(:example) do
-      sign_in @user
-    end
-
-    describe 'GET /users/:id' do
       it "displays a user's page" do
         get user_path(@user)
 
         expect(response).to have_http_status(200)
       end
     end
+  end
 
-    describe 'GET /users' do
+  describe 'GET /users' do
+    context 'when not logged in' do
+      it 'responds with redirect' do
+        get users_path
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context 'when logged in' do
+      before(:example) do
+        sign_in @user
+      end
+
       context 'when there is no other users registered' do
-        it 'renders empty page' do
+        it "doesn't display any user" do
           get users_path
 
           expect(response).to have_http_status(200)
@@ -54,7 +58,7 @@ RSpec.describe 'Users', type: :request do
           create_list(:user, @users_amount)
         end
 
-        it "renders a page with #{@users_amount} users" do
+        it "displays links to #{@users_amount} users" do
           get users_path
 
           expect(response).to have_http_status(200)
