20ad280e4d5bbc15094315fa179566e830a138c2
Anton Krestyaninov <9593795@gmail.com>
2020-09-19 05:38:11 +0300

    Провел рефакторинг тестов application-хелпера

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
-  def insert_year(obj)
+  # ===> Time related helpers <===
+  def insert_year_directive_for(obj)
     obj.created_at.year == Time.now.year ? ' ' : ' %Y '
   end
 
-  def creation_time(obj)
-    obj.created_at.strftime("%d %b#{ insert_year(obj) }at %H:%M")
+  def formatted_creation_time_of(obj)
+    obj.created_at.strftime("%d %b#{ insert_year_directive_for(obj) }at %H:%M")
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,62 +1,63 @@
-# The test suit and its helpers require refactoring
-#   because of not very desctiptive names of some variables and methods
 require 'rails_helper'
-require_relative '../support/helpers/helpers_helper.rb'
 
 RSpec.describe ApplicationHelper, type: :helper do
   include ActiveSupport::Testing::TimeHelpers
-  include_context 'time related helpers'
 
-  let!(:simple_object) { SimpleObject.new }
+  let!(:simple_object) { TimeTestingHelper::SimpleObject.new }
+  let!(:next_year)     { Time.new(simple_object.created_at.year + 1) }
 
-  describe '#insert_year(obj)' do
+  describe '#insert_year_directive_for(obj)', :time_related do
     context 'when object was created this year' do
-      it 'rerurns no directives' do # Directives that used in strftime method
+      it 'returns no directives' do # Directives that used in strftime method
         10.times do
-          travel_to random_time(simple_object.created_at,
-                                next_year_time(simple_object) - 1.second)
+          # Travel to a random time up to ending of the current year
+          travel_to random_time_between(simple_object.created_at,
+                                        next_year - 1.second)
 
-          expect(insert_year(simple_object)).to match(/ */)
+          expect(insert_year_directive_for(simple_object)).to match(/ */)
         end
       end
     end
 
     context 'when object was created in any previous year' do
-      it 'rerurns a year directive' do
-        next_year = next_year_time(simple_object)
-
+      it 'returns a year directive' do
         travel_to next_year
-        expect(insert_year(simple_object)).to include('%Y')
+
+        expect(insert_year_directive_for(simple_object)).to include('%Y')
 
         10.times do
           travel_to Time.at(next_year + rand(100_000_000))
-          expect(insert_year(simple_object)).to include('%Y')
+
+          expect(insert_year_directive_for(simple_object)).to include('%Y')
         end
       end
     end
   end
 
-  describe '#creation_time' do
-    it 'returns results with different sizes for object created this year' +
-       'and object created any other year' do
-      10.times do
-        travel_to random_time(Time.new(simple_object.created_at.year),
-                              simple_object.created_at)
-        another_object = SimpleObject.new
+  describe '#formatted_creation_time_of(obj)', :time_related do
+    context 'if 2 objects was created in the same year' do
+      it 'returns strings with the same size for each' do
+        10.times do
+          travel_to random_time_between(Time.new(simple_object.created_at.year),
+                                        next_year)
+          another_object = TimeTestingHelper::SimpleObject.new
 
-        expect(creation_time(simple_object).size)
-          .to eq(creation_time(another_object).size)
+          expect(formatted_creation_time_of(simple_object).size)
+          .to eq(formatted_creation_time_of(another_object).size)
+        end
       end
+    end
 
-      next_year = next_year_time(simple_object)
+    context 'if 2 objects was created in different years' do
+      it 'returns strings with a different size for each' do
+        10.times do
+          travel_to random_time_between(next_year,
+                                        next_year + rand(50).year)
+          another_object = TimeTestingHelper::SimpleObject.new
 
-      10.times do
-        travel_to random_time(next_year,
-                              next_year + rand(50).year)
-        another_object = SimpleObject.new
-
-        expect(creation_time(simple_object).size)
-          .not_to eq(creation_time(another_object).size)
+          expect(formatted_creation_time_of(simple_object).size)
+            .not_to eq(formatted_creation_time_of(another_object).size)
+        end
       end
     end
   end
